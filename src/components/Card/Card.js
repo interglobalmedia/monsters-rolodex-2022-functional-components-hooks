89539a9534c2a8e1f593efa0fbcd7980a623341d
Maria Campbell <interglobalmedia@gmail.com>
2022-04-19 07:05:15 -0400

    Destructure id name and email also

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,19 @@
 import classes from './Card.module.scss'
 
-const Card = ({ monster }) => (
-    
-    <li className={`${classes["card-container"]} ${monster.className}`}>
-        <figure className={`${classes["card-container__image"]} ${monster.className}`}>
-            <img src={`https://robohash.org/${monster.id}?set=set3&size=300x300`} alt="monster"/>
-            <figcaption className={`${classes["card-container__data"]} ${monster.className}`}>
-                <h2>{monster.name}</h2>
-                <p>{monster.email}</p>
-            </figcaption>
-        </figure>
-    </li>
-)
+const Card = ({ monster }) => {
+    const { id, name, email } = monster
 
+    return (
+        <li className={`${classes["card-container"]} ${monster.className}`}>
+            <figure className={`${classes["card-container__image"]} ${monster.className}`}>
+                <img src={`https://robohash.org/${id}?set=set3&size=300x300`} alt={`monster ${name}`}/>
+                <figcaption className={`${classes["card-container__data"]} ${monster.className}`}>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </figcaption>
+            </figure>
+        </li>
+    )
+}
 
 export default Card
