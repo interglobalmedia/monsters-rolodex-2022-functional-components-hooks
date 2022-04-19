9a1aa5d879980835e80bac09214dd8a9eb933107
Maria Campbell <interglobalmedia@gmail.com>
2022-04-19 06:19:37 -0400

    change props to destructured component param

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,13 +1,12 @@
 import Card from '../Card/Card'
 import classes from './CardList.module.scss'
 
-const CardList = (props) => {
-    const { filteredMonsters } = props
-    return (
-        <ul className={`${classes["card-list"]} ${props.className}`}>
-          {filteredMonsters.map(monster => (<Card className="monsters-card-container" key={monster.id} monster={monster} />))}
-        </ul>
-    )
-}
+const CardList = ({ filteredMonsters }) => (
+  
+    <ul className={`${classes["card-list"]} ${filteredMonsters.className}`}>
+      {filteredMonsters.map(monster => (<Card className="monsters-card-container" key={monster.id} monster={monster} />))}
+    </ul>
+)
+
 
 export default CardList
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,13 +1,13 @@
 import SearchBoxWrapper from '../SearchBoxWrapper/SearchBoxWrapper'
 import classes from './SearchBox.module.scss'
 
-const SearchBox = (props) => {
-    return (
-        <SearchBoxWrapper className="monsters-fieldset">
-            <h2>Search The Monsters!</h2>
-            <input className={`${classes["search-box"]} ${props.className}`} type="search" placeholder={props.placeholder} onChange={props.onChange} autoFocus={props.autofocus}/>
-        </SearchBoxWrapper>
-    )
-}
+const SearchBox = ({searchBox}) => (
+    
+    <SearchBoxWrapper className="monsters-fieldset">
+        <h2>Search The Monsters!</h2>
+        <input className={`${classes["search-box"]} ${searchBox.className}`} type="search" placeholder={searchBox.placeholder} onChange={searchBox.onChange} autoFocus={searchBox.autofocus}/>
+    </SearchBoxWrapper>
+)
+
 
 export default SearchBox
